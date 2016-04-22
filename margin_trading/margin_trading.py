e6cdce71095c5d92754aca2f854364c5e79d262b
shuqing.zhou <shuqing.zhou@chinascopefinancial.com>
2016-04-22 14:22:50 +0800

    添加定时

--- margin_trading/margin_trading.py
+++ margin_trading/margin_trading.py
@@ -17,7 +17,7 @@ def block_fs():
 
 if __name__ == '__main__':
     while 1:
-        if strftime('%H%M') not in ['0230', '0600']:
+        if strftime('%H%M') not in ['0930', '1030', '1400']:
             print strftime('%Y-%m-%d %H:%M:%S %A')
             sleep(20.0)
             continue
