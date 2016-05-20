d2ebc44ffeab3109b22daaaadcfe4ec5d1a63191
xutaoding <xutao.ding@chinascopefinancial.com>
2016-05-20 17:14:46 +0800

    change to use sqlite to store job, other than memory

--- events/aps.py
+++ events/aps.py
@@ -1,7 +1,8 @@
+import os
 import sys
 from os.path import abspath, dirname
 
-from apscheduler.jobstores.memory import MemoryJobStore
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor
 
@@ -9,9 +10,14 @@ sys.path.append(dirname(dirname(abspath(__file__))))
 
 from events.envents import EventsWords
 
+sqlite_path = dirname(abspath(__file__))
+for sql_path in os.listdir(sqlite_path):
+    if sql_path.endswith('.db'):
+        os.remove(os.path.join(sqlite_path, sql_path))
 
 jobstores = {
-    'default': MemoryJobStore()
+    # 'default': MemoryJobStore()  # `cron` fail
+    'default': SQLAlchemyJobStore(url='sqlite:///jobs.db')
 }
 
 executors = {
@@ -24,7 +30,21 @@ job_defaults = {
     'max_instances': 1
 }
 
+
+def get_events_info():
+    EventsWords().get_events_info()
+
+
 app = BlockingScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults)
-app.add_job(EventsWords().get_events_info, trigger='cron', hour='9', minute='30', second='0', misfire_grace_time=10)
+app.add_job(
+    func=get_events_info,
+    trigger='cron',
+    hour='10',
+    minute='0',
+    second='0',
+    misfire_grace_time=5
+)
+
 app.start()
+
 
