3dd69f5b1ce2050d31769e85672866c8712010ae
xutaoding <xutao.ding@chinascopefinancial.com>
2016-07-08 13:56:04 +0800

    使用sqlite定时

--- s3_loaders/jobs.py
+++ s3_loaders/jobs.py
@@ -1,11 +1,23 @@
-from apscheduler.jobstores.memory import MemoryJobStore
+import os
+from os.path import dirname, abspath
+
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor
 
 from loaders import SyncFilesLoaders
 
+
+def create_sqlite():
+    sqlite_path = dirname(abspath(__file__))
+    for sql_path in os.listdir(sqlite_path):
+        if sql_path.endswith('.db'):
+            os.remove(os.path.join(sqlite_path, sql_path))
+
+create_sqlite()
+
 jobstores = {
-    'default': MemoryJobStore()
+    'default': SQLAlchemyJobStore(url='sqlite:///jobs.db')
 }
 
 # using ThreadPoolExecutor as default other than ProcessPoolExecutor(not work) to executors
@@ -19,7 +31,12 @@ job_defaults = {
     'coalesce': False,
     'max_instances': 1
 }
+
+
+def jobs():
+    SyncFilesLoaders().get_files()
+
 app = BlockingScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults)
-app.add_job(lambda: SyncFilesLoaders().get_files(), trigger='interval', seconds=3)
+app.add_job(jobs, trigger='cron', minute='*/2')
 app.start()
 
